450d4cde9425a21f42c58d28d82b7d1b0f854ad5
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2023-09-21 20:02:53 -0300

    refactor: insere elemento

--- PPI/PPI-2023-2/exercícios/criaNovoElemento.js
+++ PPI/PPI-2023-2/exercícios/criaNovoElemento.js
@@ -1,14 +1,18 @@
-function inserirElemento() {
-    const novoElemento = document.createElement("div");
-
-    novoElemento.innerHTML = `
-      <div class="item">
-        <img src="https://br.web.img2.acsta.net/pictures/14/02/04/14/08/054280.jpg" height="150" width="150">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum eaque et sunt temporibus voluptatum, in
-          aperiam ab modi sit ipsam quasi qui hic voluptates cupiditate, dolorem debitis placeat, officia commodi.
-        </p>
-      </div>
-    `;
-    document.body.appendChild(novoElemento);
+function insereElemento() {
+  const novoElemento = document.criaElemento("div");
+  const novoItem = document.criaElemento("div");
+  const novaImagem = document.criaElemento("img");
+  const NovoParagrafo = document.criaElemento("p");
+  
+  novoElemento.classList.add("item");
+  novaImagem.src = "https://br.web.img2.acsta.net/pictures/14/02/04/14/08/054280.jpg";
+  novaImagem.height = "150";
+  novaImagem.width = "150";
+  NovoParagrafo.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum eaque et sunt temporibus voluptatum, in aperiam ab modi sit ipsam quasi qui hic voluptates cupiditate, dolorem debitis placeat, officia commodi.";
+  
+  novoItem.appendChild(novaImagem);
+  novoItem.appendChild(NovoParagrafo);
+  novoElemento.appendChild(novoItem);
+  
+  document.body.appendChild(novoElemento);
 }
