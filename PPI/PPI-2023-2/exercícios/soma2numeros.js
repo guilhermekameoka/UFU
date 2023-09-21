a33d483cb916819908bd0b9740c0e2ef9f6e9da8
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2023-09-21 16:48:53 -0300

    feat: soma 2 numeros

---
+++ PPI/PPI-2023-2/exercícios/soma2numeros.js
@@ -0,0 +1,8 @@
+var numero1 = window.prompt("Digite o primeiro número:");
+numero1 = parseInt(numero1);
+
+var numero2 = window.prompt("Digite o segundo número:");
+numero2 = parseInt(numero2);
+
+var resultado = numero1 + numero2;
+window.alert("A soma dos números é: " + resultado);
