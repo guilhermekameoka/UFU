9e8f4d7e791fe0912c224adc3ce2df32e72b12b9
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2023-11-14 23:55:59 -0300

    feat: configuração lado do servidor

--- PPI/PPI-2023-2/exercícios/ajax/backend/buscar_usuarios.php
+++ PPI/PPI-2023-2/exercícios/ajax/backend/buscar_usuarios.php
@@ -1,4 +1,8 @@
 <?php
+// configuração lado do servidor (CORS)
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 require "conexao.php";
 $pdo = mysqlConnect();
 
